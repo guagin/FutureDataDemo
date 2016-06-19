bae4a8d96c94781d26ffa3b420cf8481d7faa509
RickyChang <guagin0972@gmail.com>
2016-06-19 11:05:10 +0800

    the first version of FYK to demo the future pattern.

---
+++ FuturePatternStudy/FutureData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace FuturePatternStudy
+{
+	public class FutureData
+	{
+		int askedTime = 0;
+		int targetAskedTime = 10;
+		public FutureData()
+		{
+		}
+		public bool isReady()
+		{
+			this.askedTime++;
+			Console.WriteLine("customer ask for fucking KONAMI : " + askedTime + " times");
+			return askedTime < targetAskedTime ? false : true;
+		}
+	}
+}
+
---
+++ FuturePatternStudy/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+namespace FuturePatternStudy
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			FutureData fuckKONAMI = new FutureData();
+			ProgressChecker BuyAppleProgressChecker = new ProgressChecker(fuckKONAMI, () =>
+			{
+				Console.WriteLine("KONAMI is ready!");
+				int i = 10;
+				for (i = 10; i > 0; i--)
+				{
+					Console.WriteLine("Fuck you KONAMI!!!!");
+				}		
+			}
+				);
+			Thread FutureDataThread = new Thread(BuyAppleProgressChecker.isReady);
+			FutureDataThread.Start();
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("The ProgressCheck would check it by it self, lets do some stupid looping:");
+			int stupidLoopTimes = 20;
+
+			for (stupidLoopTimes = 20; stupidLoopTimes > 0; stupidLoopTimes--)
+			{
+				Console.WriteLine("do other things");
+				Thread.Sleep(1500);
+			}
+				
+			//while (!FutureDataThread.IsAlive){};
+		}
+
+		private class ProgressChecker
+		{
+			Action callBackWhenReady;
+			FutureData FD;
+			public ProgressChecker(FutureData FD,Action callBackWhenReady){
+				this.FD = FD;
+				this.callBackWhenReady = callBackWhenReady;
+			}
+			public void isReady()
+			{
+				while (!FD.isReady())
+				{
+					Console.WriteLine("KONAMI is not ready");
+					Thread.Sleep(1000);
+				}
+
+				callBackWhenReady();
+			}
+		}
+	}
+}
